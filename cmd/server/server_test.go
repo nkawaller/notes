65233b655a531c20b64e46f6e6a25aa209217a1d
nkawaller <kawaller@gmail.com>
2024-04-16 22:14:30 -0400

    start adding markdown file reading logic

--- cmd/server/server_test.go
+++ cmd/server/server_test.go
@@ -11,20 +11,28 @@ func TestGETPost(t *testing.T) {
 
 	server := NewServer()
 
-	t.Run("landing page returns a note", func(t *testing.T) {
-		request := newNoteRequest("/")
-		response := httptest.NewRecorder()
-		server.ServeHTTP(response, request)
-		assertStatus(t, response.Code, http.StatusOK)
-		assertResponseBody(t, response.Body.String(), "hello note server")
-	})
+	// t.Run("correct file is returned based on path", func(t *testing.T) {
+	// 	request := newNoteRequest("/note1")
+	// 	response := httptest.NewRecorder()
+	// 	server.ServeHTTP(response, request)
+	// 	assertStatus(t, response.Code, http.StatusOK)
+	// 	assertResponseBody(t, response.Body.String(), "web/content/note1.md")
+	// })
+
+	// t.Run("landing page returns a note", func(t *testing.T) {
+	// 	request := newNoteRequest("/")
+	// 	response := httptest.NewRecorder()
+	// 	server.ServeHTTP(response, request)
+	// 	assertStatus(t, response.Code, http.StatusOK)
+	// 	assertResponseBody(t, response.Body.String(), "hello note server")
+	// })
 
 	t.Run("unknown route returns a 404", func(t *testing.T) {
 		request := newNoteRequest("/unknown")
 		response := httptest.NewRecorder()
 		server.ServeHTTP(response, request)
 		assertStatus(t, response.Code, http.StatusNotFound)
-		assertResponseBody(t, response.Body.String(), "OOPs, we can't seem to find the page you're looking for")
+		assertResponseBody(t, response.Body.String(), "404 page not found\n")
 	})
 }
 
---
+++ internal/utils/utils.go
@@ -0,0 +1,18 @@
+package utils
+
+import (
+	"fmt"
+    "os"
+	"path/filepath"
+)
+
+func GetMarkdownFilePath(path string) string {
+	if path == "" {
+		return filepath.Join("web", "content", "index.md")
+	}
+	return filepath.Join("web", "content", fmt.Sprintf("%s.md", path))
+}
+
+func ReadMarkdownFile(filePath string) ([]byte, error) {
+    return os.ReadFile(filePath)
+}
