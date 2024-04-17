377b97da7af64bca546a5afc0315ac7400d59153
nkawaller <kawaller@gmail.com>
2024-04-16 21:05:32 -0400

    create router

--- cmd/server/server.go
+++ cmd/server/server.go
@@ -7,15 +7,28 @@ import (
 
 type Server struct {
 	staticHandler http.Handler
+	router        *http.ServeMux
 }
 
 func NewServer() *Server {
 	fs := http.FileServer(http.Dir("./web/static"))
 	staticHandler := http.StripPrefix("/static/", fs)
-	return &Server{staticHandler: staticHandler}
+
+	s := &Server{
+		staticHandler,
+		http.NewServeMux(),
+	}
+
+	s.router.Handle("/", http.HandlerFunc(s.indexHandler))
+
+	return s
 }
 
 func (s *Server) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	s.router.ServeHTTP(w, r)
+}
+
+func (s *Server) indexHandler(w http.ResponseWriter, r *http.Request) {
 	if r.URL.Path != "/" {
 		w.WriteHeader(http.StatusNotFound)
 		fmt.Fprint(w, "OOPs, we can't seem to find the page you're looking for")
