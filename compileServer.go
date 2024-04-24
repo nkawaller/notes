e6f8e4ec020faa2b904476e562a7918252aaccc1
nkawaller <kawaller@gmail.com>
2024-04-24 16:25:59 -0400

    create build scripts

---
+++ compileServer.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+    "fmt"
+    "os"
+    "os/exec"
+)
+
+func main() {
+
+    compileServer("./cmd/server", "./bin/runserver")
+
+}
+
+func compileServer(source, output string) {
+    cmd := exec.Command("go", "build", "-o", output, source)
+    cmd.Stdout = os.Stdout
+    cmd.Stderr = os.Stderr
+    if err := cmd.Run(); err != nil {
+        panic(err)
+    }
+    fmt.Printf("Binary created in %s", output)
+}
