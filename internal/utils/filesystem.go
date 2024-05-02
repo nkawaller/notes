67ccc46f1e6f44f1c96571da68ff564373087437
nkawaller <kawaller@gmail.com>
2024-05-02 12:51:34 -0400

    write code that creates html files

--- internal/utils/filesystem.go
+++ internal/utils/filesystem.go
@@ -5,8 +5,10 @@ import (
 )
 
 type FileSystem interface {
+	Create(name string) (*os.File, error)
 	MkdirAll(path string, perm os.FileMode) error
 	ReadFile(filename string) ([]byte, error)
+    ReadDir(name string) ([]os.DirEntry, error)
 	Stat(name string) (os.FileInfo, error)
 	GetContentRoot() string
 	GetTemplateLocation() string
@@ -17,12 +19,20 @@ type DefaultFileSystem struct {
 	TemplateLocation string
 }
 
+func (fs DefaultFileSystem) Create(name string) (*os.File, error) {
+	return os.Create(name)
+}
+
 func (fs DefaultFileSystem) MkdirAll(path string, perm os.FileMode) error {
 	return os.MkdirAll(path, perm)
 }
 
 func (fs DefaultFileSystem) ReadFile(filename string) ([]byte, error) {
 	return os.ReadFile(filename)
+}
+
+func (fs DefaultFileSystem) ReadDir(name string) ([]os.DirEntry, error) {
+    return os.ReadDir(name)
 }
 
 func (fs DefaultFileSystem) Stat(name string) (os.FileInfo, error) {
