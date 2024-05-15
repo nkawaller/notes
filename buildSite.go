b3e40158b95fdae0c1c9ebaeba8022b77b057b60
nkawaller <kawaller@gmail.com>
2024-05-14 21:07:59 -0400

    combine compileBinaries and buildSite

--- buildSite.go
+++ buildSite.go
@@ -8,9 +8,22 @@ import (
 
 func main() {
 
+    compileBinary("./cmd/server", "./bin/runserver")
+	compileBinary("./cmd/staticgen", "./bin/staticgen")
+
 	executeBinary("./bin/staticgen")
 	copyStaticFile("./web/static/output.css", "./deploy/static/output.css")
 
+}
+
+func compileBinary(source, output string) {
+	cmd := exec.Command("go", "build", "-o", output, source)
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	if err := cmd.Run(); err != nil {
+		panic(err)
+	}
+	fmt.Printf("Binary created: %s\n", output)
 }
 
 func executeBinary(file string) {
